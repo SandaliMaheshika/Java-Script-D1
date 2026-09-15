4e624e17f54cdd4e3366e2e513ebeb1f3bd15873
Sandali Maheshika <maheshikasandali691@gmail.com>
2026-09-15 12:34:27 +0530

    switch

--- js/app.js
+++ js/app.js
@@ -113,5 +113,27 @@ let marks = 70;
     }
 
 
-    
+
+let day = 2;
+let dayName;
+
+switch(day){
+    case 1:
+        dayName = "Monday";
+        break;
+
+    case 2:
+        dayName = "Tuesday";
+        break;
+
+    case 3:
+        dayName = "Wednesday";
+        break;
+
+        default:
+            break;
+
+}
+console.log("\n\n"+dayName);
+
 
