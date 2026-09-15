1c7005d8e4999e7644ead57d8f50cda4bdaa0058
Sandali Maheshika <maheshikasandali691@gmail.com>
2026-09-15 10:51:54 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+console.log("Hello icet");
+
+//alert("Hello");
+
+// " ; " eka onima na...
+console.log("\n\nHi hi hi")
+
+// data type ekk on na js wala...meken okkoma print wenwa
+let a = 10;
+console.log("\n\n"+a);
+
+
+let b = "icet";
+console.log(b);
+
+
+
+// insert karna data eke data type ek balnn gannwa (typeof).
+let c = 10;
+console.log("\n\n"+typeof c);
+
+// Array
+let customer = [];
+// customer[0] = "John";
+// customer[1] = "Jane";
+console.log("\n\n"+typeof customer);
+
+
+let customer2 = ["John", "Jane",10];
+console.log("\n\n"+ customer2);
+
+// obgect
+let customer1 = {};
